ac20358fcf6a41c9b33f06285cb3d032d7319458
rklf <11515950+rklf@users.noreply.github.com>
2024-06-19 00:15:16 +0200

    Revert "Use (modified version of) ffprobe-wasm for Deno"
    This reverts commit 8f0598825ad463a035e07bb3a9165769925a3d61.

--- src/infos.ts
+++ src/infos.ts
@@ -1,9 +1,9 @@
 import { fileTypeFromBlob } from "file-type";
 import { toBlob } from "https://deno.land/std@0.224.0/streams/mod.ts";
+import { ffprobe } from "https://deno.land/x/fast_forward/ffprobe.ts";
 import probe from "probe-image-size";
 import ytdl from "ytdl-core";
 import fetch_url_info from "./fetch_url_info.ts";
-import { getInfo } from "./getInfo.ts";
 
 export default async (req: Request) => {
   const urlParams = new URL(req.url).searchParams;
@@ -42,10 +42,10 @@ export default async (req: Request) => {
           size: fileSize,
         }))
         : await new Promise((res, rej) =>
-          getInfo(url).then(({ fileInfo: { 
-            duration, size,
-            streams: [{ width, height }],
-          }}) => res({ width, height, duration: duration / 1e+6, size })).catch(rej),
+          ffprobe(url, {}).then(({
+            format: { size },
+            streams: [{ width, height, duration }],
+          }) => res({ width, height, duration, size })).catch(rej),
         )),
       contentType: mime,
       name: url.split("/").pop()?.split(".")?.shift(),
